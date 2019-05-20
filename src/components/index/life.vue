76666f8a6618a872fa6597edecadb99c6693df95
layoung <184592775@qq.com>
2019-05-20 16:46:21 +0800

    complete line two

---
+++ src/components/index/life.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="m-life">
+        <el-row>
+            <ul class="nav">
+                <li>
+                    <router-link to="/" class="takeout">
+                        美团外卖美团
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/">
+                        猫眼电影
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/">
+                        美团酒店
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/" class="apartment">
+                        民宿／公寓
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/" class="business">
+                        商家入驻
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/">
+                        公益
+                    </router-link>
+                </li>
+            </ul>
+        </el-row>
+        <el-row>
+            <el-col :span="14">
+                <slider />
+            </el-col>
+            <el-col :span="4">
+                <div class="m-life-pic"></div>
+            </el-col>
+            <el-col :span="6">
+                <div class="m-life-login">
+                    <h4>
+                        <img src="//s0.meituan.net/bs/fe-web-meituan/2d05c2b/img/avatar.jpg" alt="">
+                    </h4>      
+                    <p class="m-life-login-name">Hi！你好</p>
+                    <p>
+                        <router-link :to="{name : 'register'}">
+                            <el-button round>注册</el-button>
+                        </router-link>
+                    </p>
+                    <p>
+                        <router-link :to="{name : 'login'}">
+                            <el-button round>立即登录</el-button>
+                        </router-link>
+                    </p>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row></el-row>
+    </div>
+</template>
+<script>
+import slider from './slider.vue'
+export default {
+    components : {
+        slider
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
+
--- src/page/index.vue
+++ src/page/index.vue
@@ -5,7 +5,7 @@
                 <m-menu />
             </el-col>
             <el-col :span="19">
-                life
+                <m-life />
             </el-col>
         </el-row>
         <el-row>
@@ -17,14 +17,17 @@
 </template>
 <script>
 import mMenu from '@/components/index/menu.vue'//引入左边菜单栏组件
+import mLife from '@/components/index/life.vue'//引入右边生活区组件
 export default {
     components : {
         mMenu,
+        mLife
     }
 }
 </script>
 <style lang="scss">
-@import '@/assets/css/index/index.scss'//引入样式 该样式是首页页面的样式
+@import '@/assets/css/index/index.scss';//引入样式 该样式是首页页面的样式
+@import '@/assets/css/index/life.scss';//引入样式 该样式是首页页面的样式
 </style>
 
 
